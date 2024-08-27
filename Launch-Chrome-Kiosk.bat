7cf4d55279da71b1b773787cd2a3a4988fa485e2
aram1122 <aram.alaverdyan@mordandat.com>
2024-08-27 10:45:28 +0400

    Add files via upload

---
+++ Launch-Chrome-Kiosk.bat
@@ -0,0 +1,4 @@
+start chrome --app="http://127.0.0.1:3000" --kiosk --window-position=-1920,0 --user-data-dir=C:\monitor1
+start chrome --app="http://127.0.0.1:3000/pair" --kiosk --window-position=1920,0 --user-data-dir=C:\monitor2
+
+
